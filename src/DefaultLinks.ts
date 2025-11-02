f0aea9e4f1e25bd68ee634403439852fbbd7e55e
Sketch <sketch@sketchni.uk>
2025-11-02 17:40:09 +0000

    Add Planka link to DefaultLinks

--- src/DefaultLinks.ts
+++ src/DefaultLinks.ts
@@ -118,4 +118,5 @@ export let dev_links = [
   { label: "Immich", href: "https://img.sketchni.uk/" },
   { label: "Mailpit", href: "https://mailpit.jinjo.lan/" },
   { label: "Transmission", href: "https://bt.jinjo.lan/" },
+  { label: "Planka", href: "https://dev.sketchni.uk" },
 ];
